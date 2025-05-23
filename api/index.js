d47933d66af5a0020290d757f8509cf253e0f3fa
prashanna ghimire <ghimireprashanna11@gmail.com>
2025-05-23 18:37:47 +0545

    add middleware and a function to handle error

--- api/index.js
+++ api/index.js
@@ -20,6 +20,16 @@ app.use(express.json())
 app.use('/api/user', userRoute);
 app.use('/api/auth', authRoute);
 
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message,
+    });
+})
+
 const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`Server running on  localhost:${PORT}`);
