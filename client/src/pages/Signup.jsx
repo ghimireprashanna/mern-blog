c239f960ec20d21af7ab343028eb8dcc506381ae
prashanna ghimire <ghimireprashanna11@gmail.com>
2025-05-25 15:06:26 +0545

    add functionality to the signup page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,8 +1,45 @@
-import { Button, TextInput } from "flowbite-react";
-import React from "react";
-import { Link } from "react-router-dom";
+import { Alert, Button, Spinner, TextInput } from "flowbite-react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Signup = () => {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [Loading, setLoading] = useState(false);
+  const navigate= useNavigate()
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage("please fill out all fields");
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if(data.success=== false){
+        return setErrorMessage(data.message)
+      }
+      setLoading(false)
+      if(res.ok){
+        navigate('/signin')
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+            setLoading(false)
+
+    }
+  };
+
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -20,7 +57,10 @@ const Signup = () => {
         </div>
         {/* Right */}
         <div className="flex-1">
-          <form className="flex max-w-md flex-col gap-4">
+          <form
+            className="flex max-w-md flex-col gap-4"
+            onSubmit={handleSubmit}
+          >
             <div>
               <label>Your username</label>
 
@@ -28,13 +68,18 @@ const Signup = () => {
                 placeholder="Username"
                 id="username"
                 type="text"
-                required
+                onChange={handleChange}
               />
             </div>
             <div>
               <label>Your email</label>
 
-              <TextInput placeholder="Email" id="email" type="email" required />
+              <TextInput
+                placeholder="Email"
+                id="email"
+                type="email"
+                onChange={handleChange}
+              />
             </div>
             <div>
               <label htmlFor="password1">Your password</label>
@@ -43,18 +88,38 @@ const Signup = () => {
                 placeholder="password"
                 id="password"
                 type="password"
-                required
+                onChange={handleChange}
               />
             </div>
-            
-            <Button type="submit" className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">Submit</Button>
-            
+
+            <Button
+              type="submit"
+              disabled={Loading}
+              className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
+            >
+              {
+                Loading ? (
+                  <>
+                  <Spinner size="sm"/>
+                  <span className="pl-3">Loading</span>
+                  </> 
+                ): 'Sign Up'
+              }
+              
+            </Button>
           </form>
 
           <div className="flex text-sm gap-2 mt-5">
-              <span>Have an account?</span>
-              <Link to={'/signin'} className='text-blue-500'>Sign In</Link>
-            </div>
+            <span>Have an account?</span>
+            <Link to={"/signin"} className="text-blue-500">
+              Sign In
+            </Link>
+          </div>
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,13 @@ import tailwindcss from '@tailwindcss/vite'
 import flowbiteReact from "flowbite-react/plugin/vite";
 
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api':{
+        target:'http://localhost:5000',
+        secure:false,
+      },
+    },
+  },
   plugins: [tailwindcss(), flowbiteReact()],
 })
