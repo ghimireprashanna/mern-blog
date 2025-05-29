eade3652181b9e29e44643ac2f6dd445d00dd7c9
prashanna ghimire <ghimireprashanna11@gmail.com>
2025-05-29 13:47:52 +0545

    add redux toolkit

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,12 +1,15 @@
 import { Alert, Button, Spinner, TextInput } from "flowbite-react";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { signInstart, signInSuccess, signInFailure } from "../redux/user/userSlice";
 
 const Signin = () => {
   const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [Loading, setLoading] = useState(false);
+  const { loading, error: errorMessage} = useSelector(state => state.user)
+  const dispatch = useDispatch()
   const navigate= useNavigate()
+  
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
@@ -15,11 +18,10 @@ const Signin = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     if ( !formData.email || !formData.password) {
-      return setErrorMessage("please fill out all fields");
+      return dispatch(signInFailure('please fill out all fields'));
     }
     try {
-      setLoading(true);
-      setErrorMessage(null);
+      dispatch(signInstart());
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -27,15 +29,15 @@ const Signin = () => {
       });
       const data = await res.json();
       if(data.success=== false){
-        return setErrorMessage(data.message)
+        dispatch(signInFailure(data.message));
       }
-      setLoading(false)
+      
       if(res.ok){
+        dispatch(signInSuccess(data))
         navigate('/')
       }
     } catch (error) {
-      setErrorMessage(error.message);
-            setLoading(false)
+      dispatch(signInFailure(error.message));
 
     }
   };
@@ -85,11 +87,11 @@ const Signin = () => {
 
             <Button
               type="submit"
-              disabled={Loading}
+              disabled={loading}
               className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
             >
               {
-                Loading ? (
+                loading ? (
                   <>
                   <Spinner size="sm"/>
                   <span className="pl-3">Loading</span>
